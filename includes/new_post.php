8831bc5b8deb6dba6eeffc618ef7a3f8acf9563c
etfluent <mike@etfluent.com>
2018-10-31 21:12:59 -0500

    removed mysqli, set db class prop priv

--- includes/new_post.php
+++ includes/new_post.php
@@ -5,8 +5,6 @@ include('../objects/post.php');
     
 // instantiate database and post object
 $db = new Database();
-// $con = mysqli_connect($db->host, $db->username, $db->password, $db->db_name);
-$con = mysqli_connect("localhost", "root", "", "Bloggo_db");
 // initialize object
 $post = new Post($db);
 // POST variables from form 
